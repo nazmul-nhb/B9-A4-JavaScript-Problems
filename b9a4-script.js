13f8c5ff81c08568da5588919ba78bbc15e4bef4
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-02-07 21:03:06 +0600

    created the folder and js file; added the pdf file

---
+++ b9a4-script.js
@@ -0,0 +1,33 @@
+function calculateMoney(ticketSale) {
+    // You have to write your code here
+}
+
+
+
+function checkName(name) {
+    //write your code here
+}
+
+
+
+
+function deleteInvalids(array) {
+    // You have to write your code here
+}
+
+
+
+
+
+function password(obj) {
+    //write your code here
+}
+
+
+function monthlySavings(arr, livingCost) {
+    // You have to write your code here
+}
+
+
+
+
