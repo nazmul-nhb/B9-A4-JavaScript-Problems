d5b4cc8b611d3d27c1c2f0738658dd72ba491d4c
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-02-07 22:07:39 +0600

    solved first 3 problems

--- b9a4-script.js
+++ b9a4-script.js
@@ -1,33 +1,69 @@
 function calculateMoney(ticketSale) {
-    // You have to write your code here
-}
+    const ticketPrice = 120;
+    const guardWages = 500;
+    const staffNumbers = 8;
+    const staffWages = 50;
 
+    if (ticketSale < 0) {
+        return `“Invalid Number” : The number of ticket sales cannot be a negative value!`;
+    }
+    else {
+        const totalEarnings = ticketPrice * ticketSale;
+        const totalExpenses = guardWages + staffNumbers * staffWages;
+        const totalSavings = totalEarnings - totalExpenses;
+        return totalSavings;
+    }
+
+}
+// console.log(calculateMoney(-93));
 
 
 function checkName(name) {
-    //write your code here
+    const goodEndings = "AaYyIiEeOoUuWw";
+
+    if(typeof name !== "string"){
+        return "invalid";
+    }
+    else if (goodEndings.includes(name[name.length - 1])) {
+        return "Good Name";
+    }
+    else {
+        return "Bad Name";
+    }
 }
 
+// console.log(checkName(199));
 
 
 
 function deleteInvalids(array) {
-    // You have to write your code here
+    const validNumbers =[];
+
+    for (let element of array ){
+        if (typeof element === "number") {
+            validNumbers.push(element);
+        }
+    }
+    return validNumbers;
 }
 
+console.log(deleteInvalids([NaN, 1, 12, 0, -1, undefined ]));
 
 
 
-
+/* 
 function password(obj) {
     //write your code here
 }
+ */
 
 
+
+/* 
 function monthlySavings(arr, livingCost) {
-    // You have to write your code here
+    
 }
-
+ */
 
 
 
