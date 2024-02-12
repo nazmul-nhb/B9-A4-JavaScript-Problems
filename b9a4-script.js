604c694e7233adccb4a910e36d9337b489c0d740
Nazmul Hassan <nazmulnhb007@yahoo.com>
2024-02-13 00:55:57 +0600

    removed semicolons and added a comment

--- b9a4-script.js
+++ b9a4-script.js
@@ -127,3 +127,5 @@ console.log(monthlySavings([1000, 2000, 3000], NaN));
 console.log(monthlySavings([1000, 2000, 2500], 5000));
 console.log(monthlySavings([900, 2700, 3400], 10000));
 console.log(monthlySavings(100, [900, 2700, 3400]));
+
+// everything is perfect  
